ec565016920d6287683536061b43598f21c1af1f
carrus2049 <70931300+carrus2049@users.noreply.github.com>
2024-05-15 14:53:22 +0800

    add system dependant script path handling

--- src/API_SM_move_abs_val_forward.lua
+++ src/API_SM_move_abs_val_forward.lua
@@ -1,4 +1,9 @@
-local script_path = debug.getinfo(1).source:match("@?(.*/)")
+local script_path = nil
+if reaper.GetOS() ==  "Win32" or reaper.GetOS() == "Win64" then
+    script_path = debug.getinfo(1).source:match("@?(.*\\)")
+else
+    script_path = debug.getinfo(1).source:match("@?(.*/)")
+end
 dofile(script_path .. "stretch_marker_adjuster.lua")
 dofile(script_path .. "configs.lua")
 
